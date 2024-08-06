1553172df93fba62a9e1283d0867b66ce3cc033c
songlongdaiduong <song.long.dai.duong@gmail.com>
2024-08-06 18:27:56 +0800

    init: Additionally set crypto state property
    Change-Id: I61f4364248c20b8206f48e77fc6275c4cbabc2f6

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1437,6 +1437,7 @@ static void SetSafetyNetProps() {
     InitPropertySet("vendor.boot.vbmeta.device_state", "locked");
     InitPropertySet("vendor.boot.verifiedbootstate", "green");
     InitPropertySet("oplusboot.verifiedbootstate", "green");
+    InitPropertySet("ro.crypto.state", "encrypted");
 }
 
 void PropertyInit() {
